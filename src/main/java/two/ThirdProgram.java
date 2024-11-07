201231ac23bc2f5cecea3654ddfeec5072a97fc9
Emir Puska <emir.puska@it-akademija.com>
2024-11-07 20:28:45 +0100

    Pokazano je kako mozete definirati binarno vrijednost cijeloh broja

---
+++ src/main/java/two/ThirdProgram.java
@@ -0,0 +1,9 @@
+package two;
+
+public class ThirdProgram {
+    public static void main(String[] args) {
+        int number = 0b11010;
+        //2 + 8 + 16
+        System.out.println(number);
+    }
+}
