29089653febdc5a0548bdec07c21ff2006e3213d
Emir Puska <emir.puska@it-akademija.com>
2024-11-07 20:34:54 +0100

    Hexadecimalno definiranje vrijednosti

--- src/main/java/two/ThirdProgram.java
+++ src/main/java/two/ThirdProgram.java
@@ -2,8 +2,14 @@ package two;
 
 public class ThirdProgram {
     public static void main(String[] args) {
-        int number = 0b11010;
+        int number = 0b11010;//binarno definiranje vrijednosti
         //2 + 8 + 16
         System.out.println(number);
+
+        double decimalniBroj = 1.234e3;
+        System.out.println(decimalniBroj);
+
+        int broj = 0x1a;
+        System.out.println(broj); // heksadecimalno
     }
 }
