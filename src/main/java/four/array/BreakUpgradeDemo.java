0e417bd227af7221e5bad62987e726e52fbc4b88
Emir Puska <emir.puska@it-akademija.com>
2024-11-14 20:33:58 +0100

    Novi zadatak. Upgrade na stari..

--- src/main/java/four/array/BreakUpgradeDemo.java
+++ src/main/java/four/array/BreakUpgradeDemo.java
@@ -1,26 +1,39 @@
 package four.array;
 
+import java.util.Random;
 import java.util.Scanner;
 
 /**
  * Puštate korisnika da unese broj.
  * Nakon toga nudite korisniku neku nagradu ako je uneseni broj sadržan u nizu 'brojevi'.
  * Ukoliko nije onda mu kažete da uplati ponovo i pokuša ponovno.
+ * <>
+ * <p>
+ * MODIFIKOVATI zadatak tako da vrijednosti elemenata u nizu 'brojevi' na svako
+ * startanje programa budu različite.
+ * Vrijednost elementa treba da bude broj od 0-1000.
+ * Random random = new Random();
+ * int slucajniBroj = random.nextInt(1000);
+ * </>
  */
-public class BreakDemo {
+public class BreakUpgradeDemo {
     public static void main(String[] args) {
-        int[] brojevi = {12, 23, 45, 6746, 2323, 1232, 23, 6, 8, 9, 10};
+        int[] brojevi = new int[11];
+        Random random = new Random();
+        int slucajniBroj = random.nextInt(1000);
+
+
         System.out.println("Unesi broj i okušaj sreću");
         int uneseniBroj = new Scanner(System.in).nextInt();
         String message = "";
-        for(int i = 0; i <brojevi.length; i++) {
+        for (int i = 0; i < brojevi.length; i++) {
             int broj = brojevi[i];
-            if(broj == uneseniBroj) {
+            if (broj == uneseniBroj) {
                 message = "Pogodili ste...";
                 break;
             }
         }
-        if(message.isBlank()){
+        if (message.isBlank()) {
             message = "Niste pogodili..";
         }
         System.out.println(message);
